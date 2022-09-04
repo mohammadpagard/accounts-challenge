3b7d48a021532bc1ac5bac728f59cd2f41f8aa19
mohammadpagard <mohammadpagard2003@gmail.com>
2022-09-04 13:16:36 +0430

    Create validations method & cleaning.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import re
 
 
 class Account:
-    first_name = input("Enter you're first name: ")
-    last_name = input("Enter you're last name: ")
+    __first_name = input("Enter you're first name: ")
+    __last_name = input("Enter you're last name: ")
 
-    def __init__(self, user_id, phone, email, username=None, password=None):
-        self.username = self.first_name + "_" + self.last_name
+    def __init__(self, user_id, phone, email, username, password=None):
+        self.username = self.__first_name + "_" + self.__last_name
         self.password = password
         self.user_id = user_id
         self.phone = phone
@@ -16,32 +16,35 @@ class Account:
         username = username.replace('_', ' ')
         return f"{self.first_name} {self.last_name}"
 
-    def show_welcome(self):
-        # Password validations
+    # Validations methods
+    def password_validate(self):
         if len(str(self.password)) < 8:
-            raise Exception("Invalid Password!")
+            raise Exception("Invalid Password")
         else:
             self.password = hash(self.password)
-            print(f"Username: {self.username}\nPassword: {self.password}")
-        # Phone number validation
-        if len(self.phone) != 11:
-            raise Exception("Invalid phone number!")
-        elif '09' not in self.phone[:2]:
-            raise Exception("Invalid phone number!")
-        else:
-            print(f"Phone number: {self.phone}")
-        # Email validation
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            print(self.password)
 
+    def phone_number_validate(self):
+        if len(str(self.phone)) != 11:
+            raise Exception("Invalid phone number")
+        elif '09' not in self.phone[:2]:
+            raise Exception("Invalid phone number")
+        else:
+            print(self.phone)
+
+    def email_validate(self):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         def check(email_address):
             if re.fullmatch(regex, email_address):
-                print(f"Email address: {self.email}")
+                print(f"email address is {self.email}")
             else:
                 raise Exception("Invalid email address")
-
-        if __name__ == '__main__':
+        if __name__ == "__main__":
             email = self.email
             check(email)
+
+    # Decorators
+    def show_welcome(self):
         # Print welcome and username validation
         print("Welcome to our site ", end="")
         for user in self.username.split('_'):
@@ -61,8 +64,8 @@ class Account:
             raise Exception("Invalid old password")
 
 
-a1 = Account(password="Kmp981234", user_id='mohammadID', phone='09138159980', email='mohammadpagard2003@gmail.com')
-# a1.verify_change_password()
+a1 = Account(username='', password="Kmp981234", user_id='mohammadID', phone='09138159980', email='mohammadpagard2003@gmail.com')
+# a1.phone_number_validate()
 a1.show_welcome()
 
 
